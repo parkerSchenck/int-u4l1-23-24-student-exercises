8cb1d3584f413fcf38bade4b4fcf7cbc882b5850
parkerSchenck <parker.schenck1@gmail.com>
2024-06-30 12:00:16 -0400

    100%

--- input.js
+++ input.js
@@ -3,19 +3,20 @@ let messages = document.querySelector(".messages");
 
 // CODE ALONG, PART 1 CONTINUED
 // 3. Declare a variable to store the input element
-
-
+let inputBox = document.querySelector(".user_input");
+//let userInput = inputBox.value;
 submitButton.addEventListener("click", function() {
     // 4. Declare a variable to store the value of the input
     // 5. Use the console to check a value is saved.
     //  - Type something and click the button to confirm it works!
-
-
+    let userComment = inputBox.value;
+    console.log(userComment);
     
 
     // CODE ALONG, PART 2
     // 6. User .innerHTML to display the value to the messages div.
     // 7. Type something and test it out.
-
+    
+    messages.innerHTML = userComment;
 
 });
